6fa22d06840277eab8d065dcf551f07ed27905c8
Henrique Sebastião <contato@henriquesebastiao.com>
2024-08-21 13:37:07 -0400

    📜 docs: Add badges to README and header information with documentation in root directory

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -5,7 +5,22 @@ from fastapi import FastAPI
 from fast_zero.routes import auth, todos, users
 from fast_zero.schemas import Message
 
-app = FastAPI()
+description = """
+Esta API foi desenvolvida durante o curso [FastAPI do Zero](https://fastapidozero.dunossauro.com/)
+#### Documentação mais legível: [Redoc](https://api.henriquesebastiao.com/redoc)
+"""
+
+app = FastAPI(
+    title='FastAPI do Zero',
+    version='dev',
+    docs_url='/',
+    description=description,
+    contact={
+        'name': 'Henrique Sebastião',
+        'email': 'contato@henriquesebastiao.com',
+        'url': 'https://github.com/henriquesebastiao',
+    },
+)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
